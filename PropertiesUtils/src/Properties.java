988c8171c06f04b25e08a5da4c926e16cf8dd129
StarkZhidian <1532033525@qq.com>
2019-05-24 19:11:48 +0800

    Properties first edition

--- PropertiesUtils/src/Properties.java
+++ PropertiesUtils/src/Properties.java
@@ -3,11 +3,14 @@ import java.util.Map;
 import java.util.Set;
 
 /**
+ * Create by StarkZhidian on 2019-5-24
+ * <p>
  * String->String 类型的属性-值处理工具
  * 属性规则：
  * key1=value1
  * key2=value2
  * ...
+ * 每一行代表一个键值对属性，example see: ../properties.txt
  */
 public class Properties {
     private static boolean DEBUG = true;
@@ -17,6 +20,12 @@ public class Properties {
     /* 属性名-值之间的映射 map */
     private PropertyMap propertyMap = new PropertyMap();
 
+    /**
+     * 从文件中加载属性键值对
+     *
+     * @param sourceFile
+     * @throws IOException
+     */
     public void load(File sourceFile) throws IOException {
         if (sourceFile == null || !sourceFile.exists()) {
             if (DEBUG) {
@@ -27,10 +36,22 @@ public class Properties {
         load0(new BufferedReader(new FileReader(sourceFile)));
     }
 
+    /**
+     * 从代表文件绝对路径中加载键值对
+     *
+     * @param sourceFilePath
+     * @throws IOException
+     */
     public void load(String sourceFilePath) throws IOException {
         load(new File(sourceFilePath));
     }
 
+    /**
+     * 从输入流中加载键值对
+     *
+     * @param sourceStream
+     * @throws IOException
+     */
     public void load(InputStream sourceStream) throws IOException {
         if (sourceStream == null) {
             if (DEBUG) {
@@ -41,6 +62,12 @@ public class Properties {
         load0(new BufferedReader(new InputStreamReader(sourceStream)));
     }
 
+    /**
+     * 从键值对数组中加载键值对，数组长度需要为偶数，奇数下标作为键，下一个偶数下标作为其对应值
+     * {"1", "2", "2", "3"}: "1"->"2", "2"->"3"
+     *
+     * @param keyValue
+     */
     public void load(String[] keyValue) {
         if (keyValue == null) {
             if (DEBUG) {
@@ -66,6 +93,12 @@ public class Properties {
         propertyMap.put(key, value);
     }
 
+    /**
+     * 将当前储存的键值对信息存入参数所代表的文件中，如果文件已存在，则其数据将被覆盖，文件不存在则会新建文件
+     *
+     * @param destFile
+     * @throws IOException
+     */
     public void save(File destFile) throws IOException {
         if (destFile == null) {
             if (DEBUG) {
@@ -87,10 +120,22 @@ public class Properties {
         writer.close();
     }
 
+    /**
+     * 将储存的键值对属性存入参数所代表的文件的绝对路径中，如果对应文件已存在，则其数据将被覆盖，文件不存在则会新建文件
+     *
+     * @param destFilePath
+     * @throws IOException
+     */
     public void save(String destFilePath) throws IOException {
         save(new File(destFilePath));
     }
 
+    /**
+     * 将存储的键值对属性写入参数所代表的输出流中
+     *
+     * @param destStream
+     * @throws IOException
+     */
     public void save(OutputStream destStream) throws IOException {
         if (destStream == null) {
             if (DEBUG) {
